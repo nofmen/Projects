e2562d3ad22f17139782ed368c13d09377d59694
Nathan <nathan@pop-os.localdomain>
2021-07-05 21:30:00 -0400

    First Commit

---
+++ scripts/stopwatch.js
@@ -0,0 +1,52 @@
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const resetBtn = document.getElementById('reset');
+const appendTens = document.getElementById('tens');
+const appendSeconds = document.getElementById('seconds');
+
+
+let seconds = 00;
+let tens = 00;
+let Interval;
+
+function startTimerHandler() {
+  Interval = setInterval(startTimer, 10);
+}
+
+function startTimer() {
+  tens++
+  if (tens <= 9) {
+    appendTens.innerHTML = '0' + tens;
+  } else if (tens >= 9) {
+    appendTens.innerHTML = tens;
+  }
+
+  if (tens > 99) {
+    tens = 00;
+    appendTens.innerHTML = '00';
+    seconds++;
+    if (seconds < 9){
+      appendSeconds.innerHTML = '0' + seconds;
+    } else if (seconds > 9) {
+      appendSeconds.innerHTML = seconds;
+    }
+  }
+}
+
+function stopTimer() {
+  clearInterval(Interval);
+}
+
+function resetTimer() {
+  stopTimer();
+
+  tens = 00;
+  appendTens.innerHTML = "00";
+
+  seconds = 00;
+  appendSeconds.innerHTML = "00";
+}
+
+startBtn.addEventListener('click', startTimerHandler);
+stopBtn.addEventListener('click', stopTimer);
+resetBtn.addEventListener('click', resetTimer);
