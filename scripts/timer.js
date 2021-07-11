2d5a8a26513e3baa250933c3f2be5b82f8f130c5
Nathan Morgan <nofmen@gmail.com>
2021-07-10 21:57:14 -0400

    timer v0.2

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,1 +1,11 @@
+const oneMinBtn = document.getElementById('1-min')
+const fiveMinBtn = document.getElementById('5-min')
+const oneSecBtn = document.getElementById('1-sec')
+const fiveSecBtn = document.getElementById('1-sec')
 const pauseBtn = document.getElementById('pause');
+
+function addOneMinHandler() {
+    
+}
+
+oneMinBtn.addEventListener('click', addOneMinHandler);
