00bfd7fbc4c390e9af3043e68163f558a0721fbb
Nathan Morgan <nofmen@gmail.com>
2021-07-12 23:17:15 -0400

    timer v0.5

--- scripts/timer.js
+++ scripts/timer.js
@@ -2,60 +2,98 @@ const oneMinBtn = document.getElementById('1-min')
 const fiveMinBtn = document.getElementById('5-min')
 const oneSecBtn = document.getElementById('1-sec')
 const fiveSecBtn = document.getElementById('5-sec')
-const startPauseBtn = document.getElementById('start-pause');
+const startBtn = document.getElementById('start');
+const pauseBtn = document.getElementById('pause');
 const resetBtn = document.getElementById('reset');
 const appendMinutes = document.getElementById('minutes');
 const appendSeconds = document.getElementById('seconds');
 
-let minRemaining = 00;
-let secRemaining = 00;
+let minRemain = 00;
+let secRemain = 00;
+let Interval;
+let isRunning = 0;
 
 function addOneMin() {
-    minRemaining++;
-    if (minRemaining <= 9) {
-        appendMinutes.innerHTML = '0' + minRemaining;
-    } else if (minRemaining > 9) {
-        appendMinutes.innerHTML = minRemaining;
+    minRemain++;
+    if (minRemain <= 9) {
+        appendMinutes.innerHTML = '0' + minRemain;
+    } else if (minRemain > 9) {
+        appendMinutes.innerHTML = minRemain;
     }
 }
 
 function addFiveMin() {
-    minRemaining += 5;
-    if (minRemaining <= 9) {
-        appendMinutes.innerHTML = '0' + minRemaining;
-    } else if (minRemaining > 9) {
-        appendMinutes.innerHTML = minRemaining;
+    minRemain += 5;
+    if (minRemain <= 9) {
+        appendMinutes.innerHTML = '0' + minRemain;
+    } else if (minRemain > 9) {
+        appendMinutes.innerHTML = minRemain;
     }
 }
 
 function addOneSec() {
-    secRemaining++;
-    if (secRemaining <= 9) {
-        appendSeconds.innerHTML = '0' + secRemaining;
-    } else if (secRemaining > 9) {
-        appendSeconds.innerHTML = secRemaining;
+    secRemain++;
+    if (secRemain <= 59) {
+        if (secRemain <= 9) {
+            appendSeconds.innerHTML = '0' + secRemain;
+        } else if (secRemain > 9) {
+            appendSeconds.innerHTML = secRemain;
+        }
+    } else if (secRemain >= 59) {
+        secRemain = '00';
+        minRemain++;
+        if (minRemain <= 9) {
+                appendMinutes.innerHTML = '0' + minRemain;
+            } else {
+            appendMinutes.innerHTML = minRemain;
+            }
+        appendSeconds.innerHTML = secRemain;
     }
 }
 
 function addFiveSec() {
-    secRemaining += 5;
-    if (secRemaining <= 9) {
-        appendSeconds.innerHTML = '0' + secRemaining;
-    } else if (secRemaining > 9) {
-        appendSeconds.innerHTML = secRemaining;
+    secRemain += 5;
+    if (secRemain <= 9) {
+        appendSeconds.innerHTML = '0' + secRemain;
+    } else if (secRemain > 9) {
+        appendSeconds.innerHTML = secRemain;
     }
 }
 
-//function startPauseHandler() {
-//
-//}
+function startHandler() {
+    if (isRunning == 0) {
+        isRunning++;
+        Interval = 0;
+        Interval = setInterval(startTimer, 1000);
+    } else {
+        // do nothing
+    }
+}
+
+function startTimer() {
+    secRemain--
+    if (secRemain >= 10) {
+        appendSeconds.innerHTML = secRemain;
+    } else if (secRemain < 10) {
+        appendSeconds.innerHTML = '0' + secRemain;
+    }
+
+    console.log('interval ' + Interval);
+}
 
 function resetTimer() {
-    minRemaining = 00;
-    secRemaining = 00;
+    minRemain = 00;
+    secRemain = 00;
 
     appendMinutes.innerHTML = '00';
     appendSeconds.innerHTML = '00';
+
+    stopTimer();
+}
+
+function stopTimer() {
+    clearInterval(Interval);
+    isRunning = 0;
 }
 
 oneMinBtn.addEventListener('click', addOneMin);
@@ -63,4 +101,4 @@ fiveMinBtn.addEventListener('click', addFiveMin);
 oneSecBtn.addEventListener('click', addOneSec);
 fiveSecBtn.addEventListener('click', addFiveSec);
 resetBtn.addEventListener('click', resetTimer);
-//startPauseBtn.addEventListener('click', startPauseHandler);
+startBtn.addEventListener('click', startHandler);
