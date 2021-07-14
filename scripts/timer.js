22441d5171b9cd89009073947d7517243e50b573
Nathan Morgan <nofmen@gmail.com>
2021-07-13 21:51:35 -0400

    timer v 1.0

--- scripts/timer.js
+++ scripts/timer.js
@@ -7,28 +7,19 @@ const pauseBtn = document.getElementById('pause');
 const resetBtn = document.getElementById('reset');
 const appendMinutes = document.getElementById('minutes');
 const appendSeconds = document.getElementById('seconds');
+const beep = new Audio('sound_effects/beep.wav');
 
 let minRemain = 00;
 let secRemain = 00;
 let Interval;
 let isRunning = 0;
 
-function increaseMinutes() {
+function udpateMinutes() {
     if (minRemain <= 9) {
         appendMinutes.innerHTML = '0' + minRemain;
     } else if (minRemain > 9) {
         appendMinutes.innerHTML = minRemain;
     }
-}
-
-function addOneMin() {
-    minRemain++;
-    increaseMinutes();
-}
-
-function addFiveMin() {
-    minRemain += 5;
-    increaseMinutes();
 }
 
 function increaseSeconds() {
@@ -41,10 +32,19 @@ function increaseSeconds() {
     } else if (secRemain >= 59) {
         secRemain = 00;
         minRemain++;
-        increaseMinutes();
+        udpateMinutes();
         appendSeconds.innerHTML = '00';
     }
-    console.log(secRemain + ' ' + minRemain);
+}
+
+function addOneMin() {
+    minRemain++;
+    udpateMinutes();
+}
+
+function addFiveMin() {
+    minRemain += 5;
+    udpateMinutes();
 }
 
 function addOneSec() {
@@ -65,16 +65,36 @@ function startHandler() {
     }
 }
 
+function decreaseSeconds() {
+    secRemain--;
+    if (secRemain >= 10) {
+        appendSeconds.innerHTML = secRemain;
+    } else if (secRemain < 10) {
+        appendSeconds.innerHTML = '0' + secRemain;
+    }
+}
+
 function startTimer() {
-    if (minRemain === 00 && secRemain === 00) {
-        pauseTimer();
-        resetTimer();
-    } else {
-        secRemain--
-        if (secRemain >= 10) {
+    if (minRemain > 00) {
+        if (secRemain >= 01){
+            decreaseSeconds();
+        } else if (secRemain == 00) {
+            minRemain--
+            udpateMinutes();
+            secRemain = 59;
             appendSeconds.innerHTML = secRemain;
-        } else if (secRemain < 10) {
-            appendSeconds.innerHTML = '0' + secRemain;
+        }
+    } else if (minRemain == 00) {
+        if (secRemain == 01) {
+            pauseTimer();
+            resetTimer();
+            beep.play();
+            document.getElementById('timer-page-body').style.backgroundColor = "red";
+            setTimeout(() => {
+                document.getElementById('timer-page-body').style.backgroundColor = "white";
+            }, 1000);
+        } else if (secRemain > 01) {
+            decreaseSeconds();
         }
     }
 }
